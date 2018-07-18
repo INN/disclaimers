4a09c7ab714b7aad78d5b81efd5f52cad4850c76
Ben Keith <ben@benlk.com>
2018-07-17 20:00:04 -0400

    Make the metabox actually display the settting.

--- includes/metaboxes.php
+++ includes/metaboxes.php
@@ -3,6 +3,7 @@
  * Post Meta box functions for the Disclaimer widget's per-post disclaimer functionality.
  */
 namespace INN\Disclaimers\Metaboxes;
+use Disclaimers;
 
 // Disclaimer
 add_action( 'widgets_init', function() {
@@ -10,36 +11,37 @@ add_action( 'widgets_init', function() {
 	largo_add_meta_box(
 		'largo_custom_disclaimer',
 		__( 'Disclaimer', 'largo' ),
-		'largo_custom_disclaimer_meta_box_display', //could also be added with largo_add_meta_content('largo_custom_related_meta_box_display', 'largo_additional_options')
+		__NAMESPACE__ . '\largo_custom_disclaimer_meta_box_display', //could also be added with largo_add_meta_content('largo_custom_related_meta_box_display', 'largo_additional_options')
 		'post',
 		'normal',
 		'core'
 	);
 
-	/**
-	 * Disclaimer text area for the Additional Options metabox
-	 *
-	 * If the post's disclaimer field is empty, then the default disclaimer 
-	 * is the option set in the theme options.
-	 *
-	 * @global $post
-	 */
-	function largo_custom_disclaimer_meta_box_display() {
-		global $post;
-
-		$value = get_post_meta( $post->ID, 'disclaimer', true );
-
-		if ( empty( $value ) ) {
-			$value = get_option( 'default_disclaimer' );
-		}
-
-		echo '<p><strong>' . __('Disclaimer', 'largo') . '</strong><br />';
-		echo '<textarea name="disclaimer" style="width: 98%;">' . esc_textarea( $value ) . '</textarea>';
-
-	}
 	largo_register_meta_input( 'disclaimer', 'wp_filter_post_kses' );
-
 } );
+/**
+ * Disclaimer text area for the Additional Options metabox
+ *
+ * If the post's disclaimer field is empty, then the default disclaimer 
+ * is the option set in the theme options.
+ *
+ * @global $post
+ */
+function largo_custom_disclaimer_meta_box_display() {
+	global $post;
+
+	$value = get_post_meta( $post->ID, 'disclaimer', true );
+
+	echo '<p><strong>' . __('Disclaimer', 'largo') . '</strong><br />';
+	echo '<textarea name="disclaimer" style="width: 98%;">' . esc_textarea( $value ) . '</textarea>';
+	printf(
+		'<p>%1$s</p><blockquote>%2$s</blockquote>',
+		esc_html__( 'The default disclaimer used on this site is:', 'disclaimers' ),
+		wp_kses_post( get_option( Disclaimers::OPTION_KEY ) )
+	);
+
+}
 
 // @todo: port the above to standard wordpress-y of doing things
 // make sure it works with and without largo
+
